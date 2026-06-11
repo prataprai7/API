5d0f48504fa1f9688d1c039d900761e3ea95df5f
pratap <pratapbantawa3@gmail.com>
2026-06-11 07:48:15 +0545

    feat: update user controller and routes

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,10 +1,19 @@
 import { UserController } from "../controllers/user.controller";
 import { Router } from "express";
+import { authorizedMiddleware } from "../middleware/authorized.middleware";
+import { uploads } from "../middleware/upload.middleware";
 
 const userController = new UserController();
 const router = Router();
 
 router.post("/register", userController.createUser);
 router.post("/login", userController.loginUser);
+
+router.post(
+    "/update",
+    authorizedMiddleware, //user should be logged in
+    uploads.single("profileImage"), //multer for file upload -> req.file
+    userController.updateUser
+);
 
 export default router;
